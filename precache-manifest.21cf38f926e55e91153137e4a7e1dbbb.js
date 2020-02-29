5da5ca3f66e470939830ec5bdc2a0de9ee7c72f1
palmaone <isra.palma@gmail.com>
2020-02-29 11:53:34 -0600

    Updates

---
+++ precache-manifest.21cf38f926e55e91153137e4a7e1dbbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104c5d772ebf8c26038c1d8469917aa1",
+    "url": "/monsters-rolodex-learn-react/index.html"
+  },
+  {
+    "revision": "a1abba15f45d74e43dd0",
+    "url": "/monsters-rolodex-learn-react/static/css/main.5b5886c3.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/monsters-rolodex-learn-react/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex-learn-react/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1abba15f45d74e43dd0",
+    "url": "/monsters-rolodex-learn-react/static/js/main.70fcb05e.chunk.js"
+  },
+  {
+    "revision": "ab97f89623304dee2e2b",
+    "url": "/monsters-rolodex-learn-react/static/js/runtime-main.439b527c.js"
+  }
+]);
